c8e2f19fc8d9b03913776824bc7b8471acd237a0
StewartRowney <110835640+StewartRowney@users.noreply.github.com>
2024-01-24 10:42:44 +0000

    Initial

---
+++ src/test/java/com/example/museums/MuseumsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.museums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
